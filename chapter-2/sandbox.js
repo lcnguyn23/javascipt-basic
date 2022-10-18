60b9f2a30ad0f4f9cea00ed407641b5f260d1c52
Loc Nguyen <89457284+lcnguyn23@users.noreply.github.com>
2022-10-18 09:42:37 +0700

    18/10

--- chapter-2/sandbox.js
+++ chapter-2/sandbox.js
@@ -524,3 +524,23 @@ console.log(Math.trunc(area)); // 5.3 -> 5
 // random numbers
 const random1 = Math.random();
 console.log(Math.round(random1 * 100));
+
+/////////////////
+// primitive values
+let scoreOne = 50;
+let scoreTwo = scoreOne;
+
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+scoreOne = 100;
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// reference values
+
+const userOne = { name: "ryu", age: 30 };
+const userTwo = userOne;
+
+console.log(userOne, userTwo);
+
+userOne.name = "chun-li";
+console.log(userOne, userTwo);

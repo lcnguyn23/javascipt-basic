3fe1b806cbedf1412f7a6c5c4303ac5bf450ffa4
Loc Nguyen <89457284+lcnguyn23@users.noreply.github.com>
2022-10-21 16:25:28 +0700

    vid13

--- chapter-2/sandbox2.js
+++ chapter-2/sandbox2.js
@@ -112,28 +112,56 @@
 //   console.log("you clicked me");
 // });
 
-const ul = document.querySelector("ul");
+// const ul = document.querySelector("ul");
 // ul.remove();
 
-const button = document.querySelector("button");
+// const button = document.querySelector("button");
 
-button.addEventListener("click", () => {
-  const li = document.createElement("li");
-  li.textContent = "something new to do";
-  // ul.innerHTML += '<li>something new</li>'
-  // ul.append(li);
-  // ul.prepend(li);
+// button.addEventListener("click", () => {
+//   const li = document.createElement("li");
+//   li.textContent = "something new to do";
+//  // ul.innerHTML += '<li>something new</li>'
+//  // ul.append(li);
+//   ul.prepend(li);
+// });
+
+// const items = document.querySelectorAll("li");
+
+// items.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     // console.log("item clicked");
+//     // console.log(e);
+//     // console.log(e.target);
+//     // console.log(item);
+//     // e.target.style.textDecoration = "line-through";
+//     console.log("event in LI");
+//     e.stopPropagation();
+//     e.target.remove();
+//   });
+// });
+
+// ul.addEventListener("click", (e) => {
+//   // console.log("event in UL");
+//   if (e.target.tagName === "LI") {
+//     e.target.remove();
+//   }
+// });
+
+const copy = document.querySelector(".copy-me");
+
+copy.addEventListener("copy", () => {
+  console.log("OI! my content is copyright");
 });
 
-const items = document.querySelectorAll("li");
+const box = document.querySelector(".box");
 
-items.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    // console.log("item clicked");
-    // console.log(e);
-    // console.log(e.target);
-    // console.log(item);
-    // e.target.style.textDecoration = "line-through";
-    e.target.remove();
-  });
+box.addEventListener("mousemove", (e) => {
+  // console.log(e);
+  // console.log(e.offsetX, e.offsetY);
+  box.textContent = `x pos -${e.offsetX} y pos - ${e.offsetY}`;
 });
+
+// document.addEventListener("wheel", (e) => {
+//   console.log(e.pageX, e.pageY);
+// });
+
